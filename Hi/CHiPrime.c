ebaef3ad21adfdcbd46218b0dcc4d3c4a2c2c097
alexy2008 <alexy2008@163.com>
2020-10-24 11:45:35 +0800

    优化C/C++程序

--- Hi/CHiPrime.c
+++ Hi/CHiPrime.c
@@ -13,7 +13,7 @@ llong _prime[150000];
 llong _maxPrime;
 
 void primeByEuler(int page) {
-    bool num[page];
+    bool *num = (bool *)malloc(sizeof(bool)*page);
     for (int i = 0; i < page; i++) num[i] = false;
     for (int i = 2; i < page; i++) {
         if (!num[i]) {
@@ -28,10 +28,10 @@ void primeByEuler(int page) {
     free(num);
 }
 
-void PrimeByEratosthenes(llong pos, int page) {
-    bool num[page];
+void primeByEratosthenes(llong pos, int page) {
+    bool *num = (bool *)malloc(sizeof(bool)*page);
     for (int i = 0; i < page; i++) num[i] = false;
-    for (int i = 0; _prime[i] < sqrt(pos + page); i++) {
+    for (int i = 0; _prime[i] * _prime[i] < pos + page; i++) {
         llong p = _prime[i];
         for (llong j = ceil(pos * 1.0 / p) * p; j < pos + (llong) page; j += p)
             num[(int) (j - pos)] = true;
@@ -39,9 +39,14 @@ void PrimeByEratosthenes(llong pos, int page) {
     for (int i = 0; i < page; i++)
         if (!num[i]) {
             _maxPrime = pos + i;
-            _prime[_maxInd++ - _offSet] = _maxPrime;
+            _maxInd++;
         }
     free(num);
+}
+
+void sieve(llong limit, int page){
+    primeByEuler(page);
+    for (int i = 1; i < limit/page; i++) primeByEratosthenes(page * (long) i, page);
 }
 
 int main(int argc, char *argv[]) {
@@ -50,11 +55,7 @@ int main(int argc, char *argv[]) {
     const llong PAGE = atoll(argv[2]);
     printf("Calculate prime numbers up to %lld using partitioned Eratosthenic sieve \n", LIMIT);
     llong startTime = clock();
-    primeByEuler(PAGE);
-    for (int i = 1; i < LIMIT/PAGE; i++) {
-        PrimeByEratosthenes(PAGE * (llong) i, PAGE);
-        if (_maxInd > _maxKeep) _offSet = _maxInd - _maxKeep;
-    }
+    sieve(LIMIT, PAGE);
     llong totalTime = clock() - startTime;
     printf("C finished within %.0e; the %lldth prime is %lld, time cost: %lld ms \n",
            (double) LIMIT, _maxInd, _maxPrime, totalTime);
