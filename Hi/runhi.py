335b96c5faa59230069ad917602c68ed7f8156fe
alexy2008 <alexy2008@163.com>
2020-12-29 19:02:54 +0800

    完善运行脚本

--- Hi/runhi.py
+++ Hi/runhi.py
@@ -1,22 +1,55 @@
 # coding=utf-8
 import platform
 import click
-# import colorama
+import subprocess
+import os
+import glob
+
+command = {'java': {'ver': 'java -version',
+                    'build': 'javac -version && javac JHiPrime.java -d bin',
+                    'run': 'java -cp ./bin JHiPrime %s %s'},
+           'c': {'ver': 'gcc --version',
+                 'build': 'gcc CHiPrime.c -lm -O3 -o ./bin/CHiPrime',
+                 'run': './bin/CHiPrime %s %s'}}
+
+is_windows = True
+osname = 'windows'
+launch = ''
+launch_cmd = 'for /l %%i in (1,1,%s) do @%s'
+launch_sh = 'for i in $(seq %s);do %s;done'
+
+def check(lang):
+    curpath = '.'
+    if len(glob.glob(r'%s' % lang))>0 :
+        curpath = './%s' % lang
+
+    if len(glob.glob(r'%s/*Hello*' % lang))>0 :
+        print(glob.glob(r'%s/*Hello*' % lang))
 
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
 
 
 @click.group()
 def cli():
-    click.secho('欢迎HiPrime CLI！', fg='blue', bg='black')
+    global is_windows, osname, launch
+    osname = platform.system()
+    is_windows = (osname == 'Windows')
+
+    if is_windows:
+        launch = launch_cmd
+    else:
+        launch = launch_sh
+
+    g = glob.glob(r'*Hi*')
+    print(len(g))
+
+    print(os.getcwd())  # 获取当前工作目录路径
+    print(os.path.abspath('.'))  # 获取当前工作目录路径
+    print(os.path.abspath('src/bin'))  # 获取当前目录文件下的工作目录路径
+    print(os.path.abspath('..'))  # 获取当前工作的父目录 ！注意是父目录路径
+    print(os.path.abspath(os.curdir))  # 获取当前工作目录路径
+
+    click.secho('欢迎HiPrime CLI for %s' % osname, fg='blue', bg='black')
     click.secho('https://www.deepinjava.com', fg='yellow', underline=True)
     pass
 
@@ -24,7 +57,12 @@ def cli():
 @cli.command(help='编译源代码')
 @click.argument('lang')
 def build(lang):
-    click.echo('开始编译'+lang)
+    click.echo('开始编译%s' % lang)
+    p = subprocess.Popen(command[lang]['build'], shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    while p.poll() is None:
+        out = p.stdout.readline()
+        print(out, end="")
+    click.echo('%s编译完成' % lang)
 
 
 @cli.command(help='运行程序')
@@ -36,18 +74,20 @@ def build(lang):
 @click.option('--repeat', '-r', default=1, help='执行次数')
 @click.option('--docker', '-d', help='使用docker运行')
 def run(lang, limit, page, mode, thread, repeat, docker):
+    check(lang)
     click.secho('limit to: %s' % limit, fg='red', bg='black')
+    c = command[lang]['run'] % (limit, page)
+    if is_windows:
+        c = c.replace('/', '\\')
+
+    c = launch % (repeat, c)
+    c = command[lang]['ver'] + ' && ' + c
+    print(c)
+    p = subprocess.Popen(c, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+    while p.poll() is None:
+        out = p.stdout.readline()
+        print(out, end="")
 
 
 if __name__ == '__main__':
     cli()
-
-print(platform.system())
-
-if platform.system() == 'Windows':
-    print('Windows系统')
-else:
-    if platform.system() == 'Linux':
-        print('Linux系统')
-    else:
-        print('其他')
