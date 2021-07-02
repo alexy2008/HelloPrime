b403aa87a1013cb04477e81ecc995fa69959b676
Alex <alexy2008@!63.com>
2021-07-02 10:47:13 +0800

    更新了java16支持

--- Hi/Java15/JHiPrime.java
+++ Hi/Java15/JHiPrime.java
@@ -1,33 +1,35 @@
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
 public class JHiPrime {
     private static final int maxKeep = 350000;
     private static CountDownLatch cd;
 
-    private static Result primeByEuler(Integer page) {
+    private static ArrayList<Long> primeByEuler(Integer page) {
         var num = new boolean[page];
-        long[] primeArray = new long[maxKeep];
-        long maxInd = 0, maxPrime = 0;
+        ArrayList<Long> primeArray = new ArrayList<>(maxKeep);
+        long maxPrime = 0;
         for (var i = 2; i < page; i++) {
             if (!num[i]) {
                 maxPrime = i;
-                primeArray[(int) (maxInd++)] = maxPrime;
+                primeArray.add(maxPrime);
+//                primeArray[(int) (maxInd++)] = maxPrime;
             }
-            for (var j = 0; (long) i * primeArray[j] < page; j++) {
-                num[(int) (i * primeArray[j])] = true;
-                if (i % primeArray[j] == 0) break;
+            for (var j = 0; (long) i * primeArray.get(j) < page; j++) {
+                num[(int) (i * primeArray.get(j))] = true;
+                if (i % primeArray.get(j) == 0) break;
             }
         }
-        return new Result(maxInd, maxPrime, primeArray);
+        return primeArray;
     }
 
-    private static Result primeByEratosthenes(Long pos, Integer page, long[] primeArray) {
+    private static Result primeByEratosthenes(Long pos, Integer page, ArrayList<Long> primeArray) {
         var num = new boolean[page];
         long maxInd = 0, maxPrime = 0;
-        for (var i = 0; primeArray[i] * primeArray[i] < pos + page; i++) {
-            var p = primeArray[i];
+        for (var i = 0; primeArray.get(i) * primeArray.get(i) < pos + page; i++) {
+            var p = primeArray.get(i);
             for (var j = ((long) (Math.ceil((double) pos / p))) * p; j < pos + page; j += p) {
-                System.out.println("i is:"+i+";p is:"+p +" ;j is :" + j + ";j - pos is:" + (j-pos));
+//                System.out.println("i is:"+i+";p is:"+p +" ;j is :" + j + ";j - pos is:" + (j-pos));
                 num[(int) (j - pos)] = true;
             }
         }
@@ -36,27 +38,26 @@ public class JHiPrime {
                 maxPrime = pos + i;
                 maxInd++;
             }
-        return new Result(maxInd, maxPrime, null);
+        return new Result(maxInd, maxPrime);
     }
 
     public static Result sieve(long limit, int page) {
-        Result r = primeByEuler(page);
-        long maxInd = r.maxInd, maxPrime = r.maxPrime;
-        long[] primeArray = r.primeArray;
+        ArrayList<Long> li = primeByEuler(page);
+        long maxInd = li.size(), maxPrime = li.get((int) maxInd - 1);
+        Result r;
         for (var i = 1; i < limit / page; i++) {
-            r = primeByEratosthenes(page * (long) i, page, primeArray);
+            r = primeByEratosthenes(page * (long) i, page, li);
             maxInd+= r.maxInd;
             maxPrime = r.maxPrime;
         }
-        return new Result(maxInd, maxPrime, primeArray);
+        return new Result(maxInd, maxPrime);
     }
 
     public static Result sieve(long limit, int page, int threadCount) throws InterruptedException {
         if (threadCount <= 1) return sieve(limit, page);
         cd = new CountDownLatch(threadCount);
-        Result r = primeByEuler(page);
-        long maxInd = r.maxInd, maxPrime = r.maxPrime;
-        long[] primeArray = r.primeArray;
+        ArrayList<Long> li = primeByEuler(page);
+        long maxInd = li.size(), maxPrime = li.get((int) maxInd - 1);
 
         class Task extends Thread{
             public long maxPrime;
@@ -69,7 +70,7 @@ public class JHiPrime {
             public void run() {
                 for (int i = tid ; i < limit/page ; i+= threadCount) {
                     if (i==0) continue;
-                    var rs = primeByEratosthenes(page * (long) i, page, primeArray );
+                    var rs = primeByEratosthenes(page * (long) i, page, li );
                     maxPrime = rs.maxPrime;
                     maxInd += rs.maxInd;
                 }
@@ -87,10 +88,10 @@ public class JHiPrime {
             if (ths[i].maxPrime > maxPrime) maxPrime = ths[i].maxPrime;
             maxInd += ths[i].maxInd;
         }
-        return new Result(maxInd, maxPrime, primeArray);
+        return new Result(maxInd, maxPrime);
     }
 
-    public record Result(long maxInd, long maxPrime, long[] primeArray){}
+    public record Result(long maxInd, long maxPrime){}
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hi Prime! I'm Java :-)");
--- tools/sjdk.ps1
+++ tools/sjdk.ps1
@@ -1,4 +1,5 @@
 switch ($args[0]) {
+    "16" {$jhome = "D:\jdk16"}
     "15" {$jhome = "D:\jdk15"}
     "14" {$jhome = "D:\jdk14"}
     "11" {$jhome = "C:\Program Files\Java\jdk-11.0.2"}
