200eb4f9e4891a906854c658230b3d7280d59569
Alex Young <alexy2008@163.com>
2023-08-22 21:02:10 +0800

    修改优化Python结构

--- Hi/PyHiPrime.py
+++ Hi/PyHiPrime.py
@@ -2,29 +2,23 @@ import math
 import sys
 import time
 
-max_keep = 80000
-prime_array = []
-max_ind = 0
-max_prime = 0
-
-
 def prime_by_euler(page):
-    global max_ind, max_prime, prime_array
-
+    prime_array = []
     num = [False] * page
     for i in range(2, page):
         if not num[i]:
-            max_prime = i
-            prime_array.append(max_prime)
-            max_ind = max_ind + 1
+            prime_array.append(i)
         for j in range(0, len(prime_array)):
             if i * prime_array[j] >= page: break
             num[i * prime_array[j]] = True
             if i % prime_array[j] == 0: break
+    
+    return prime_array
 
 
-def prime_by_eratosthenes_interval(pos, page):
-    global max_ind, max_prime, prime_array
+def prime_by_eratosthenes_interval(pos, page,prime_array):
+    max_ind = 0
+    max_prime = 0
 
     num = [False] * page
     for i in range(0, len(prime_array)):
@@ -36,16 +30,21 @@ def prime_by_eratosthenes_interval(pos, page):
     for i in range(0, page):
         if not num[i]:
             max_prime = pos + i
-            # prime_array.append(max_prime)
             max_ind = max_ind + 1
+    
+    return max_ind,max_prime
 
 
 def sieve(limit, page):
-    prime_by_euler(page)
+    prime_array = prime_by_euler(page)
+    max_ind = len(prime_array)
+    max_prime = prime_array[max_ind - 1]
     for i in range(1, int(limit / page)):
-        prime_by_eratosthenes_interval(page * i, page)
-        # if max_ind > max_keep:
-        #     del prime_array[max_keep:len(prime_array)]
+        lind,lp = prime_by_eratosthenes_interval(page * i, page, prime_array)
+        max_ind = max_ind + lind
+        max_prime = lp
+    
+    return max_ind,max_prime
 
 
 def main():
@@ -55,7 +54,7 @@ def main():
     print("Calculate prime numbers up to ", limit, " using partitioned Eratosthenes sieve", flush=True)
 
     start_time = time.time()
-    sieve(limit, page)
+    max_ind, max_prime = sieve(limit, page)
     total_time = round((time.time() - start_time) * 1000)
     print("Python finished within %.0e" % float(limit), "the %d" % max_ind, 'th prime is %d' % max_prime,
           'time cost: %d' % total_time, 'ms', flush=True)
