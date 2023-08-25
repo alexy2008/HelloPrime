a0af79760d26aa1378369f7853f2e433123e37ff
Alex Young <alexy2008@163.com>
2023-08-25 18:04:42 +0800

    优化java代码

--- Hello/Java/JHelloPrime.java
+++ Hello/Java/JHelloPrime.java
@@ -8,10 +8,12 @@ public class JHelloPrime {
         var sieve = new boolean[page];
         ArrayList<Long> primeArray = new ArrayList<>();
         for (var i = 2; i < page; i++) {
-            if (!sieve[i]) primeArray.add((long) i);
+            if (!sieve[i])
+                primeArray.add((long) i);
             for (var j = 0; (long) i * primeArray.get(j) < page; j++) {
                 sieve[(int) (i * primeArray.get(j))] = true;
-                if (i % primeArray.get(j) == 0) break;
+                if (i % primeArray.get(j) == 0)
+                    break;
             }
         }
         return primeArray;
@@ -19,11 +21,12 @@ public class JHelloPrime {
 
     private static Result primeByEratosthenes(Long pos, Integer page, ArrayList<Long> primeArray) {
         var sieve = new boolean[page];
-        long maxInd = 0, maxPrime = 0;
-        for (var i = 1; i < primeArray.size() && primeArray.get(i) * primeArray.get(i) < pos + page; i++) {
+        long maxInd = 0, maxPrime = 0, sqrLimit = (long) Math.ceil(Math.sqrt((pos + page)));
+        for (var i = 1; i < primeArray.size() && primeArray.get(i) < sqrLimit; i++) {
             var p = primeArray.get(i);
             // for (var j = ((long) (Math.ceil((double) pos / p))) * p; j < pos + page; j += p)
-            for (var j = (pos % p == 0 ? pos / p : (pos / p) + 1) * p; j < pos + page; j += p)
+            // for (var j = (pos % p == 0 ? pos / p : (pos / p) + 1) * p; j < pos + page; j+= p)
+            for (var j = ((pos + p - 1) / p) * p; j < pos + page; j += p)
                 sieve[(int) (j - pos)] = true;
         }
         for (var i = 1; i < page; i += 2)
@@ -31,7 +34,7 @@ public class JHelloPrime {
                 maxPrime = pos + i;
                 maxInd++;
             }
-            return new Result(maxInd, maxPrime);
+        return new Result(maxInd, maxPrime);
     }
 
     public static Result calculate(Long limit, Integer page, Integer threadNumber) throws InterruptedException {
@@ -40,9 +43,8 @@ public class JHelloPrime {
         ArrayList<Long> primerList = primeByEuler(page * n);
         AtomicLong maxInd = new AtomicLong(primerList.size());
         AtomicLong maxPrime = new AtomicLong(primerList.get((int) maxInd.get() - 1));
-        if (isDebug) System.out.println("Info=>" + "|" + (page * n) + "|" + maxInd + "|" + maxPrime+ "|");
+        if (isDebug) System.out.println("Info=>" + "|" + (page * n) + "|" + maxInd + "|" + maxPrime + "|");
         Thread[] task = new Thread[threadNumber];
-
         for (int i = 0; i < threadNumber; i++) {
             int tid = i, finalN = n;
             task[tid] = new Thread(() -> {
@@ -50,16 +52,53 @@ public class JHelloPrime {
                 for (int j = tid + finalN; j < limit / page; j += threadNumber) {
                     var rs = primeByEratosthenes(page * (long) j, page, primerList);
                     localMaxPrime = rs.maxPrime;
-                    localMaxInd += rs.maxInd; 
-                    if (isDebug) System.out.println("Info=>|T"+ tid + "|" + ((long)page * (j+1)) + "|" + localMaxInd + "|" + localMaxPrime+ "|");
+                    localMaxInd += rs.maxInd;
+                    if (isDebug)
+                        System.out.println("Info=>|T" + tid + "|" + ((long) page * (j + 1)) + "|" + localMaxInd + "|"
+                                + localMaxPrime + "|");
                 }
-                if ((tid + 1) % threadNumber == ((limit / page) - finalN) % threadNumber) maxPrime.set(localMaxPrime);
+                if (((tid + 1) % threadNumber == ((limit / page) - finalN) % threadNumber) && (localMaxPrime > maxPrime.get()))
+                    maxPrime.set(localMaxPrime);
                 maxInd.addAndGet(localMaxInd);
             });
             task[tid].start();
         }
-        for (Thread t : task) t.join();
+        for (Thread t : task)
+            t.join();
         return new Result(maxInd.get(), maxPrime.get());
+    }
+
+    public static Result SieveOfAtkin(int limit) {
+        long maxPrime = 5, maxInd = 2;
+        System.out.println("Atk");
+        boolean sieve[] = new boolean[limit + 1];
+        int up = (int)Math.sqrt(limit);
+
+        for (int x = 1; x  <= up; x++) {
+            for (int y = 1; y  <= up; y++) {
+                int n = (4 * x * x) + (y * y);
+                if (n <= limit && (n % 12 == 1 || n % 12 == 5))
+                    sieve[n] ^= true;
+                n = (3 * x * x) + (y * y);
+                if (n <= limit && n % 12 == 7)
+                    sieve[n] ^= true;
+                n = (3 * x * x) - (y * y);
+                if (x > y && n <= limit && n % 12 == 11)
+                    sieve[n] ^= true;
+            }
+        }
+        for (int r = 5; r * r <= limit; r++) {
+            if (sieve[r]) {
+                for (int i = r * r; i <= limit; i += r * r)
+                    sieve[i] = false;
+            }
+        }
+        for (int a = 0; a <= limit; a++)
+            if (sieve[a]){
+                maxInd ++;
+                maxPrime = a;
+            }
+        return new Result(maxInd, maxPrime);           
     }
 
     public record Result(long maxInd, long maxPrime) {}
@@ -72,7 +111,15 @@ public class JHelloPrime {
         int threadNumber = Integer.parseInt(args[3]);
         System.out.println("Calculate prime numbers up to " + limit + " using partitioned Eratosthenes sieve");
         var startTime = System.currentTimeMillis();
-        Result r = calculate(limit, page, threadNumber);
+        Result r ;
+        if (page == 0) {
+            r = SieveOfAtkin((int)limit);  
+            
+        }
+        else{
+            r = calculate(limit, page, threadNumber);  
+        }
+        
         var totalTime = System.currentTimeMillis() - startTime;
         System.out.printf("Java using %d thread(s) finished within %.0e the %dth prime is %d, time cost: %d ms \n",
                 threadNumber, (double) limit, r.maxInd, r.maxPrime, totalTime);
